ef4fe4f8a7a04b0a50f18ab527390d997d89a5c2
Vinícius Domingos Gabriel <vinidomgabriel@gmail.com>
2024-07-05 12:55:18 -0300

    c

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/ui_telaavaliacao.h
@@ -0,0 +1,132 @@
+/********************************************************************************
+** Form generated from reading UI file 'telaavaliacao.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TELAAVALIACAO_H
+#define UI_TELAAVALIACAO_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_telaAvaliacao
+{
+public:
+    QLabel *label_2;
+    QPushButton *btn_enviar;
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QRadioButton *a1;
+    QRadioButton *a2;
+    QRadioButton *a3;
+    QRadioButton *a4;
+    QRadioButton *a5;
+
+    void setupUi(QDialog *telaAvaliacao)
+    {
+        if (telaAvaliacao->objectName().isEmpty())
+            telaAvaliacao->setObjectName("telaAvaliacao");
+        telaAvaliacao->resize(381, 206);
+        label_2 = new QLabel(telaAvaliacao);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(80, 20, 231, 51));
+        QFont font;
+        font.setPointSize(21);
+        label_2->setFont(font);
+        btn_enviar = new QPushButton(telaAvaliacao);
+        btn_enviar->setObjectName("btn_enviar");
+        btn_enviar->setGeometry(QRect(100, 160, 171, 31));
+        layoutWidget = new QWidget(telaAvaliacao);
+        layoutWidget->setObjectName("layoutWidget");
+        layoutWidget->setGeometry(QRect(90, 110, 221, 20));
+        horizontalLayout_2 = new QHBoxLayout(layoutWidget);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(layoutWidget);
+        label->setObjectName("label");
+
+        horizontalLayout_2->addWidget(label);
+
+        label_3 = new QLabel(layoutWidget);
+        label_3->setObjectName("label_3");
+
+        horizontalLayout_2->addWidget(label_3);
+
+        label_4 = new QLabel(layoutWidget);
+        label_4->setObjectName("label_4");
+
+        horizontalLayout_2->addWidget(label_4);
+
+        label_5 = new QLabel(layoutWidget);
+        label_5->setObjectName("label_5");
+
+        horizontalLayout_2->addWidget(label_5);
+
+        label_6 = new QLabel(layoutWidget);
+        label_6->setObjectName("label_6");
+
+        horizontalLayout_2->addWidget(label_6);
+
+        a1 = new QRadioButton(telaAvaliacao);
+        a1->setObjectName("a1");
+        a1->setGeometry(QRect(90, 80, 16, 20));
+        a2 = new QRadioButton(telaAvaliacao);
+        a2->setObjectName("a2");
+        a2->setGeometry(QRect(130, 80, 16, 20));
+        a3 = new QRadioButton(telaAvaliacao);
+        a3->setObjectName("a3");
+        a3->setGeometry(QRect(180, 80, 16, 20));
+        a4 = new QRadioButton(telaAvaliacao);
+        a4->setObjectName("a4");
+        a4->setGeometry(QRect(220, 80, 16, 20));
+        a5 = new QRadioButton(telaAvaliacao);
+        a5->setObjectName("a5");
+        a5->setGeometry(QRect(270, 80, 16, 20));
+
+        retranslateUi(telaAvaliacao);
+
+        QMetaObject::connectSlotsByName(telaAvaliacao);
+    } // setupUi
+
+    void retranslateUi(QDialog *telaAvaliacao)
+    {
+        telaAvaliacao->setWindowTitle(QCoreApplication::translate("telaAvaliacao", "Dialog", nullptr));
+        label_2->setText(QCoreApplication::translate("telaAvaliacao", "Avalie sua estadia:", nullptr));
+        btn_enviar->setText(QCoreApplication::translate("telaAvaliacao", "Enviar Avalia\303\247\303\243o", nullptr));
+        label->setText(QCoreApplication::translate("telaAvaliacao", "1", nullptr));
+        label_3->setText(QCoreApplication::translate("telaAvaliacao", "2", nullptr));
+        label_4->setText(QCoreApplication::translate("telaAvaliacao", "3", nullptr));
+        label_5->setText(QCoreApplication::translate("telaAvaliacao", "4", nullptr));
+        label_6->setText(QCoreApplication::translate("telaAvaliacao", "5", nullptr));
+        a1->setText(QString());
+        a2->setText(QString());
+        a3->setText(QString());
+        a4->setText(QString());
+        a5->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class telaAvaliacao: public Ui_telaAvaliacao {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TELAAVALIACAO_H
---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/ui_telacolaborador.h
@@ -0,0 +1,150 @@
+/********************************************************************************
+** Form generated from reading UI file 'telacolaborador.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TELACOLABORADOR_H
+#define UI_TELACOLABORADOR_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_telaColaborador
+{
+public:
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLabel *txt_nome;
+    QSpacerItem *horizontalSpacer;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QPushButton *btn_cadastroQuarto;
+    QPushButton *btn_listaQuartos;
+    QPushButton *btn_reservas;
+    QPushButton *btn_listaClientes;
+    QPushButton *btn_cadastrarColaborador;
+    QPushButton *btn_notificacoes;
+
+    void setupUi(QDialog *telaColaborador)
+    {
+        if (telaColaborador->objectName().isEmpty())
+            telaColaborador->setObjectName("telaColaborador");
+        telaColaborador->resize(649, 403);
+        layoutWidget = new QWidget(telaColaborador);
+        layoutWidget->setObjectName("layoutWidget");
+        layoutWidget->setGeometry(QRect(40, 20, 561, 35));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(layoutWidget);
+        label->setObjectName("label");
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Bahnschrift SemiBold SemiConden")});
+        font.setPointSize(20);
+        font.setBold(true);
+        label->setFont(font);
+
+        horizontalLayout->addWidget(label);
+
+        txt_nome = new QLabel(layoutWidget);
+        txt_nome->setObjectName("txt_nome");
+        txt_nome->setFont(font);
+
+        horizontalLayout->addWidget(txt_nome);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        widget = new QWidget(telaColaborador);
+        widget->setObjectName("widget");
+        widget->setGeometry(QRect(20, 63, 601, 321));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        btn_cadastroQuarto = new QPushButton(widget);
+        btn_cadastroQuarto->setObjectName("btn_cadastroQuarto");
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Minimum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(btn_cadastroQuarto->sizePolicy().hasHeightForWidth());
+        btn_cadastroQuarto->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_cadastroQuarto);
+
+        btn_listaQuartos = new QPushButton(widget);
+        btn_listaQuartos->setObjectName("btn_listaQuartos");
+        sizePolicy.setHeightForWidth(btn_listaQuartos->sizePolicy().hasHeightForWidth());
+        btn_listaQuartos->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_listaQuartos);
+
+        btn_reservas = new QPushButton(widget);
+        btn_reservas->setObjectName("btn_reservas");
+        sizePolicy.setHeightForWidth(btn_reservas->sizePolicy().hasHeightForWidth());
+        btn_reservas->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_reservas);
+
+        btn_listaClientes = new QPushButton(widget);
+        btn_listaClientes->setObjectName("btn_listaClientes");
+        sizePolicy.setHeightForWidth(btn_listaClientes->sizePolicy().hasHeightForWidth());
+        btn_listaClientes->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_listaClientes);
+
+        btn_cadastrarColaborador = new QPushButton(widget);
+        btn_cadastrarColaborador->setObjectName("btn_cadastrarColaborador");
+        sizePolicy.setHeightForWidth(btn_cadastrarColaborador->sizePolicy().hasHeightForWidth());
+        btn_cadastrarColaborador->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_cadastrarColaborador);
+
+        btn_notificacoes = new QPushButton(widget);
+        btn_notificacoes->setObjectName("btn_notificacoes");
+        sizePolicy.setHeightForWidth(btn_notificacoes->sizePolicy().hasHeightForWidth());
+        btn_notificacoes->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(btn_notificacoes);
+
+
+        retranslateUi(telaColaborador);
+
+        QMetaObject::connectSlotsByName(telaColaborador);
+    } // setupUi
+
+    void retranslateUi(QDialog *telaColaborador)
+    {
+        telaColaborador->setWindowTitle(QCoreApplication::translate("telaColaborador", "Dialog", nullptr));
+        label->setText(QCoreApplication::translate("telaColaborador", "Bem vindo ", nullptr));
+        txt_nome->setText(QCoreApplication::translate("telaColaborador", "Nome", nullptr));
+        btn_cadastroQuarto->setText(QCoreApplication::translate("telaColaborador", "Cadastrar novo quarto", nullptr));
+        btn_listaQuartos->setText(QCoreApplication::translate("telaColaborador", "Lista de quartos", nullptr));
+        btn_reservas->setText(QCoreApplication::translate("telaColaborador", "Reservas", nullptr));
+        btn_listaClientes->setText(QCoreApplication::translate("telaColaborador", "Lista de clientes", nullptr));
+        btn_cadastrarColaborador->setText(QCoreApplication::translate("telaColaborador", "Cadastrar novo colaborador", nullptr));
+        btn_notificacoes->setText(QCoreApplication::translate("telaColaborador", "Notifica\303\247\303\265es", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class telaColaborador: public Ui_telaColaborador {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TELACOLABORADOR_H
